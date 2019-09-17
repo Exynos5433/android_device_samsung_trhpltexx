41ad03b020d1cbac0231e9217050423e995d54a0
ananjaser1211 <ananjaser@gmail.com>
2019-09-17 10:47:42 +0400

    [TEMP]trhpltexx: assert treltexx

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter trhpltexx, $(TARGET_DEVICE)),)
+ifneq ($(filter trhpltexx,treltexx, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
