7d8dd7e8b627161b0a168634e5deafbb5cc90942
AnanJaser <ananjaser@gmail.com>
2020-11-21 21:45:52 +0400

    trhpltexx: Havoc-ify
    Co-Authored-By: ripee <ripee.dev@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The Havoc-OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_trhpltexx.mk \
-    $(LOCAL_DIR)/rr_trhpltexx.mk
+    $(LOCAL_DIR)/havoc_trhpltexx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_trhpltexx-user \
-    lineage_trhpltexx-userdebug \
-    lineage_trhpltexx-eng \
-    rr_trhpltexx-userdebug
+    havoc_trhpltexx-user \
+    havoc_trhpltexx-userdebug \
+    havoc_trhpltexx-eng
