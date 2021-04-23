b2daed2afd6c8997c11dfe3c101a583ed119ba3b
Anan Jaser <ananjaser@gmail.com>
2021-04-23 21:29:55 +0400

    trhpltexx: do not override fingerprint or target
    * these are now handled by init

--- lineage_trhpltexx.mk
+++ lineage_trhpltexx.mk
@@ -31,10 +31,5 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N910U
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-BUILD_FINGERPRINT := samsung/trhpltexx/trelte:6.0.1/MMB29K/N910CXXU2DSA2:user/release-keys
 BOARD_VENDOR := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=trhpltexx \
-	TARGET_DEVICE=trhpltexx \
-  	PRIVATE_BUILD_DESC="trhpltexx-user 6.0.1 MMB29K N910CXXU2DSA2 release-keys"
